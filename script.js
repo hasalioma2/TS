f7d3d03e9f4b8e7491422979488f89b6b04bb962
hasalioma2 <56221788+hasalioma2@users.noreply.github.com>
2020-05-31 14:56:24 +0300

    initial
    initial

---
+++ script.js
@@ -0,0 +1,10 @@
+function funct1(param1, param2) {
+    console.log(param1);
+    if (param2 != "undefined") {
+        console.log("Ther para 5");
+    }
+    else {
+        console.log(param2);
+    }
+}
+funct1("ALi");
---
+++ script.ts
@@ -0,0 +1,10 @@
+function funct1(param1:string, param2?:string){
+    console.log(param1)
+    if(param2 !="undefined"){
+        console.log("Ther para 5");
+    } else{
+        console.log(param2)
+    }
+}
+
+funct1("ALi")
